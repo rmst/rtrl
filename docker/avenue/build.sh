10bf033978764d5fcfc54b8dfb6d6e7ab2b5a812
Simon Ramstedt <simonramstedt@gmail.com>
2019-09-11 14:10:44 -0400

    add dockerfiles

--- docker/avenue/build.sh
+++ docker/avenue/build.sh
@@ -5,20 +5,19 @@ find_master_rev(){
   rm -rf $tempdir
 }
 
+set -e  # exit on error
+
 AVENUE_REV=${1:-$(find_master_rev git@github.com:ElementAI/Avenue.git)}
 echo "Building with Avenue revision $AVENUE_REV"
 
-git clone git@github.com:ElementAI/Avenue.git /app/avenue
+git clone git@github.com:ElementAI/Avenue.git avenue
 pushd avenue
-  git pull
   git reset --hard ${AVENUE_REV}
-  pip install -e .
 popd
 
-docker build -t unity3d - < unity3d.dockerfile
+docker build -t unity3d - < unity3d.dockerfile  # TODO: this image should be temporary
 docker build -t unity3d-py --build-arg BASE=unity3d:latest --build-arg AVENUE_REV=$AVENUE_REV ../pytorch
-docker build -t avenue --build-arg BASE=unity3d-py:latest . 
-docker tag avenue:latest images.borgy.elementai.net/rmst/avenue:latest
-docker push images.borgy.elementai.net/rmst/avenue:latest
+docker build -t $DOCKER_REMOTE/avenue:$AVENUE_REV --build-arg BASE=unity3d-py:latest . 
+docker push $DOCKER_REMOTE/avenue:$AVENUE_REV
 
 rm -rf avenue
