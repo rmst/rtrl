79e66be586357e51c20b2fa2c6e030cc7346c6e7
Simon Ramstedt <simonramstedt@gmail.com>
2019-10-18 03:20:39 -0400

    normalize_env

--- rtrl/envs.py
+++ rtrl/envs.py
@@ -19,17 +19,18 @@ def mujoco_py_issue_424_workaround():
 
 
 def normalize_half_cheetah(env):
-  mean = np.array((0.22799526154994965, 1.5053091049194336, 0.2822640538215637, 0.28506359457969666, 0.2727450430393219,
-         0.38986486196517944, 0.33656004071235657, 0.28865376114845276, 0.7166818976402283, 0.761592447757721,
-         1.6132901906967163, 5.744279384613037, 6.705165863037109, 7.640653610229492, 6.6329426765441895,
-         7.241889476776123, 6.1558427810668945))
-  std = np.array((0.051981840282678604, 2.2659554481506348, 0.07967299222946167, 0.08126124739646912,
-         0.07438986003398895, 0.15199460089206696, 0.11327265202999115, 0.0833209902048111, 0.5136329531669617,
-         0.5800230503082275, 2.602705240249634, 32.99674606323242, 44.959251403808594, 58.37958908081055,
-         43.99592971801758, 52.44496536254883, 37.8943977355957))
+  mean = np.array([-2.8978434e-01,  1.6660135e+00,  8.3246939e-02,  8.7178364e-02,
+        2.4026206e-02,  6.5018900e-02,  1.4149654e-02,  1.0042821e-01,
+       -6.2972151e-02, -5.7678702e-03,  3.1061701e-02,  2.9805478e-02,
+       -6.7784175e-02,  4.7515810e-02, -5.1149428e-03,  5.3308241e-02,
+        3.0382004e-04])
+  std = np.array([0.23879883, 1.4998492 , 0.28086397, 0.29111406, 0.28239846,
+       0.36456883, 0.3396931 , 0.28372395, 0.673492  , 0.7025841 ,
+       1.4711759 , 5.872539  , 6.899377  , 7.830265  , 6.6277347 ,
+       7.3030405 , 6.1115613 ])
 
   env = AffineObservationWrapper(env, -mean, 1/std)
-  env = AffineRewardWrapper(env, 0., 1.)
+  # env = AffineRewardWrapper(env, 0., 1.)
   return env
 
 
@@ -38,12 +39,14 @@ class GymEnv(gym.Wrapper):
   seed_val: InitVar[int]  # the name seed is already taken by the gym.Env.seed function
   id: str = "Pendulum-v0"
   real_time: bool = False
+  normalize: bool = False
 
   def __post_init__(self, seed_val):
     env = gym.make(self.id)
-    # if self.id.startswith("HalfCheetah"):
-    #   # env = normalize_half_cheetah(env)
-    #   env = AffineObservationWrapper(env, 0., 0.1)
+    if self.normalize:
+      assert self.id.startswith("HalfCheetah")
+      env = normalize_half_cheetah(env)
+      # env = AffineObservationWrapper(env, 0., 0.1)
 
     env = Float64ToFloat32(env)
     env = TimeLimitResetWrapper(env)
