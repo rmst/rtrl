264b72926bf7648f4c04c21bb0f6ea7a1c75b75b
Simon Ramstedt <simonramstedt@gmail.com>
2019-10-26 05:25:29 -0400

    fix_test_memory_issues

--- rtrl/testing.py
+++ rtrl/testing.py
@@ -1,8 +1,9 @@
 import atexit
 import multiprocessing as mp
+from copy import deepcopy
 from dataclasses import dataclass
 from functools import partial
-
+import torch
 import pandas as pd
 from pandas import DataFrame
 
@@ -11,21 +12,22 @@ from rtrl.wrappers import StatsWrapper
 
 
 class Test:
-  def __init__(self, number: int = 2, **kwargs):
+  def __init__(self, actor, number: int = 1, device: str = 'cpu', **kwargs):
+    # cuda requires ~2GB RAM per process just to start so we might want to test on cpu even if cuda is available.
+    actor = deepcopy(actor).to(device)
+
     # Note: It is important that we `spawn` here. Using the default `fork`, will cause Pytorch 1.2 to lock up because it uses a buggy OpenMPI implementation (libgomp). Olexa Bilaniuk at Mila helped us figure this out.
-    self.pool = mp.get_context('spawn').Pool(number)
-    self.result_handle = self.pool.map_async(partial(run_test, **kwargs), range(number))
+    self.pool = mp.get_context('spawn').Pool(max(number, 1))
+    self.result_handle = self.pool.map_async(partial(run_test, actor=actor, **kwargs), range(number))
 
   def stats(self):
     st = self.result_handle.get()
     st = DataFrame(st)
     means = st.mean(skipna=True)
     # stds = st.std(skipna=True).add_suffix("std")
-    return means
-
-  def __del__(self):
     self.pool.close()
     self.pool.join()
+    return means
 
 
 def run_test(number, *, Env, actor, base_seed, steps):
